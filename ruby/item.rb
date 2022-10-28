a6deae477fb8afb13d24c43dae880a9e11e11aae
Michael Stack <michael.stack@gmail.com>
2022-10-28 09:16:42 -0600

    update Inventory and Item
    Item is replaced with a new module providing distinct item type classes and
    a factory function to build the new item classes

---
+++ ruby/item.rb
@@ -0,0 +1,101 @@
+require_relative './item_pricing'
+require_relative './item_expiration'
+
+module Item
+
+  # Factory function to build a new item
+  #
+  # @param [string] name
+  # @param [string] sell_by
+  # @param [string] price
+  # @return [Item::FineArt, Item::ConcertTickets, Item::GoldCoins, Item::NormalItem]
+  def create(name, sell_by, price)
+    case name
+    when 'Fine Art'
+      FineArt.new(name, sell_by, price)
+    when 'Concert Tickets'
+      ConcertTickets.new(name, sell_by, price)
+    when 'Gold Coins'
+      GoldCoins.new(name, sell_by, price)
+    else
+      NormalItem.new(name, sell_by, price)
+    end
+  end
+
+  module_function :create
+
+  class Base
+    include Pricing
+    include Expiration
+
+    attr_accessor :name, :sell_by, :price
+
+    def initialize(name, sell_by, price)
+      @name = name
+      @sell_by = sell_by
+      @price = price
+    end
+
+    # End-of-day item attribute processing
+    #
+    def end_of_day_update
+      @sell_by -= 1 if expires?
+      pricing.each do |strategy|
+        strategy.apply(self)
+      end
+      @price = [min_price, [max_price, @price].min].max
+    end
+  end
+
+  # Concert Tickets
+  #
+  # Expires normally
+  # Appreciates by +1 normally,
+  # by +2 within 10 days of the concert,
+  # by +3 within 5 days of the concert
+  # Depreciates to 0 after the concert
+  # Price range is [0..50] inclusive
+  class ConcertTickets < Base
+    appreciates 1
+    appreciates 1, after: 10
+    appreciates 1, after: 5
+    depreciates Float::INFINITY, after: 0
+  end
+
+  # Gold Coins
+  #
+  # Does not expire
+  # Does not appreciate
+  # Does not depreciate
+  # Price range is [80..80] inclusive
+  class GoldCoins < Base
+    does_not_expire
+
+    has_price_minimum 80
+    has_price_maximum 80
+  end
+
+  # Fine Art
+  #
+  # Expires normally
+  # Appreciates by +1 normally,
+  # by +2 after sell by expiration
+  # Does not depreciate
+  # Price range is [0..50] inclusive
+  class FineArt < Base
+    appreciates 1
+    appreciates 1, after: 0
+  end
+
+  # Normal Items
+  #
+  # Expires normally
+  # Does not appreciate
+  # Depreciates by -1 normally,
+  # by -2 after sell by expiration
+  # Price range is [0..50] inclusive
+  class NormalItem < Base
+    depreciates 1
+    depreciates 1, after: 0
+  end
+end
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,10 +1,3 @@
-PRICE_MAXIMUM = 50
-PRICE_MINIMUM = 0
-
-SELL_BY_PAST = 0
-SELL_BY_NEAR = 6
-SELL_BY_FAR = 11
-
 class Inventory
 
   def initialize(items)
@@ -13,63 +6,7 @@ class Inventory
 
   def update_price
     @items.each do |item|
-      if item.name != "Fine Art" and item.name != "Concert Tickets"
-        if item.price > PRICE_MINIMUM
-          if item.name != "Gold Coins"
-            item.price = item.price - 1
-          end
-        end
-      else
-        if item.price < PRICE_MAXIMUM
-          item.price = item.price + 1
-          if item.name == "Concert Tickets"
-            if item.sell_by < SELL_BY_FAR
-              if item.price < PRICE_MAXIMUM
-                item.price = item.price + 1
-              end
-            end
-            if item.sell_by < SELL_BY_NEAR
-              if item.price < PRICE_MAXIMUM
-                item.price = item.price + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Gold Coins"
-        item.sell_by = item.sell_by - 1
-      end
-      if item.sell_by < SELL_BY_PAST
-        if item.name != "Fine Art"
-          if item.name != "Concert Tickets"
-            if item.price > PRICE_MINIMUM
-              if item.name != "Gold Coins"
-                item.price = item.price - 1
-              end
-            end
-          else
-            item.price = item.price - item.price
-          end
-        else
-          if item.price < PRICE_MAXIMUM
-            item.price = item.price + 1
-          end
-        end
-      end
+      item.end_of_day_update
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_by, :price
-
-  def initialize(name, sell_by, price)
-    @name = name
-    @sell_by = sell_by
-    @price = price
-  end
-
-  def to_s
-    "#{@name}, #{@sell_by}, #{@price}"
   end
 end
