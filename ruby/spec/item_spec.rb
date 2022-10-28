5ab7efae91703df7db8d98d643ba920d6b81a265
Michael Stack <michael.stack@gmail.com>
2022-10-28 10:57:10 -0600

    add tests to explicitly cover unknown/unhandled items

--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -130,4 +130,35 @@ describe :Item do
       end
     end
   end
+
+  describe :UnknownItems do
+    let(:item_name) { "Unknown Item" }
+
+    [
+      # reduces price and sell_by for unknown items
+      {
+        given: { sell_by: 10, price: 20 },
+        want: { sell_by: 9, price: 19 },
+      },
+      # reduces price 2x as fast for unknown items past sell_by
+      {
+        given: { sell_by: -1, price: 20 },
+        want: { sell_by: -2, price: 18 },
+      },
+      # does not allow price to go negative
+      {
+        given: { sell_by: 10, price: 0 },
+        want: { sell_by: 9, price: 0 },
+      }
+    ].each_with_index do |test_case, i|
+      context "TestCase[#{i}] given #{test_case[:given]}" do
+        it "returns #{test_case[:want]}" do
+          item = Item.create(item_name, test_case[:given][:sell_by], test_case[:given][:price])
+          item.end_of_day_update
+          expect(item.sell_by).to eq(test_case[:want][:sell_by])
+          expect(item.price).to eq(test_case[:want][:price])
+        end
+      end
+    end
+  end
 end
